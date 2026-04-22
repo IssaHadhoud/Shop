ff164b8fcbb09c07b1df53bccffe0159ea9464ad
IssaHadhoud <issaAbuHahdoud@gmail.com>
2026-04-22 16:53:25 +0300

    cose

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,10 @@ const dns = require("node:dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const db = require("./models/db");
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 app.use("/uploads",express.static("uploads"))
 const productRouter=require("./routers/product")
 app.use("/product",productRouter)
